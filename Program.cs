8e359132d2df962557063b9dc1c2eb64c063048f
Levi <lnatamm25@gmail.com>
2024-12-01 15:46:55 -0300

    Changing instantiation of cards

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@
 
 List<Card> allCards = new List<Card>()
 {
-    new ResourceCard(age:1, name:"Lumber Yard", type:"Raw Material", minimumPlayers:3, resources:new List<string>(){"Wood"}),
-    new ResourceCard(age:1, name:"Lumber Yard", type:"Raw Material", minimumPlayers:4, resources:new List<string>(){"Wood"}),
-    new ResourceCard(age:1, name:"Stone Pit", type:"Raw Material", minimumPlayers:3, resources:new List<string>(){"Stone"}),
-    new ResourceCard(age:1, name:"Stone Pit", type:"Raw Material", minimumPlayers:5 , resources:new List<string>(){"Stone"}),
-    new ResourceCard(age:1, name:"Clay Pool", type:"Raw Material", minimumPlayers:3 , resources:new List<string>(){"Clay"}),
-    new ResourceCard(age:1, name:"Clay Pool", type:"Raw Material", minimumPlayers:5 , resources:new List<string>(){"Clay"}),
-    new ResourceCard(age:1, name:"Ore Vein", type:"Raw Material", minimumPlayers:3 , resources:new List<string>(){"Ore"}),
-    new ResourceCard(age:1, name:"Ore Vein", type:"Raw Material", minimumPlayers:4 , resources:new List<string>(){"Ore"}),
-    new ResourceCard(age:1, name:"Tree Farm", type:"Raw Material", minimumPlayers:6 , resources:new List<string>(){"Wood", "Clay"}),
-    new ResourceCard(age:1, name:"Excavation", type:"Raw Material", minimumPlayers:4 , resources:new List<string>(){"Stone", "Clay"}),
-    new ResourceCard(age:1, name:"Clay Pit", type:"Raw Material", minimumPlayers:3 , resources:new List<string>(){"Clay", "Ore"}),
+    new ResourceCard(age:1, name:"Lumber Yard", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3, resources:new List<string>(){"Wood"}),
+    new ResourceCard(age:1, name:"Lumber Yard", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:4, resources:new List<string>(){"Wood"}),
+    new ResourceCard(age:1, name:"Stone Pit", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3, resources:new List<string>(){"Stone"}),
+    new ResourceCard(age:1, name:"Stone Pit", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:5 , resources:new List<string>(){"Stone"}),
+    new ResourceCard(age:1, name:"Clay Pool", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3 , resources:new List<string>(){"Clay"}),
+    new ResourceCard(age:1, name:"Clay Pool", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:5 , resources:new List<string>(){"Clay"}),
+    new ResourceCard(age:1, name:"Ore Vein", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3 , resources:new List<string>(){"Ore"}),
+    new ResourceCard(age:1, name:"Ore Vein", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:4 , resources:new List<string>(){"Ore"}),
+    new ResourceCard(age:1, name:"Tree Farm", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:6 , resources:new List<string>(){"Wood", "Clay"}),
+    new ResourceCard(age:1, name:"Excavation", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:4 , resources:new List<string>(){"Stone", "Clay"}),
+    new ResourceCard(age:1, name:"Clay Pit", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3 , resources:new List<string>(){"Clay", "Ore"}),
 };
 
 bool ValidateRange(int x, int op)
