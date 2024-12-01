7fc3a855772a8bad14d9cbb9fe907f260ba319ce
Levi <lnatamm25@gmail.com>
2024-12-01 15:45:21 -0300

    Adding "Cost" attribute

--- Models/Card.cs
+++ Models/Card.cs
@@ -5,6 +5,7 @@ public class Card
     private int _age;
     private string _name;
     private string _type;
+    private List<(string, int)> _cost;
     private int _minimumPlayers;
 
     public int Age
@@ -22,16 +23,22 @@ public class Card
         get => _type;
     }
 
+    public List<(string, int)> Cost
+    {
+        get => _cost;
+    }
+    
     public int MinimumPlayers
     {
         get => _minimumPlayers;
     }
     
-    public Card(int age, string name, string type, int minimumPlayers)
+    public Card(int age, string name, string type, List<(string, int)> cost, int minimumPlayers)
     {
         _age = age;
         _name = name;
         _type = type;
+        _cost = cost;
         _minimumPlayers = minimumPlayers;
     }
     
