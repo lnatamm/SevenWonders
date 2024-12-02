bb65aeeb6ee11ce20e2d62b3fa4d5413cd0d97b4
Levi <lnatamm25@gmail.com>
2024-12-01 23:17:37 -0300

    Creating wonders and implementing GetPlayers() method

--- Program.cs
+++ Program.cs
@@ -15,6 +15,17 @@ List<Card> allCards = new List<Card>()
     new ResourceCard(age:1, name:"Clay Pit", type:"Raw Material", cost: new List<(string, int)>(){("Wood", 3), ("Coin", 2)}, minimumPlayers:3 , resources:new List<string>(){"Clay", "Ore"}),
 };
 
+List<AbstractWonder> wonders = new List<AbstractWonder>()
+{
+    new Alexandria(),
+    new Babylon(),
+    new Ephesos(),
+    new Gizah(),
+    new Halikarnassos(),
+    new Olympia(),
+    new Rhodos()
+};
+
 bool ValidateRange(int x, int op)
 {
     switch (op)
@@ -37,6 +48,17 @@ int InputInt(int op)
         return InputInt(op);
     }
     return value;
+}
+
+string InputString(int op)
+{
+    string value;
+    if (op == 1)
+    {
+        value = Console.ReadLine();
+        return value;
+    }
+    return "";
 }
 
 void Print(Object message)
@@ -67,6 +89,27 @@ void AddCardsToDeck(Deck deck, List<Card> cards, int age, int numberOfPlayers)
     }
 }
 
+T Draw<T>(IList<T> list)
+{
+    T result = list[0];
+    list.RemoveAt(0);
+    return result;
+}
+
+List<Player> GetPlayers(int numberOfPlayers)
+{
+    List<Player> players = new List<Player>();
+    Shuffler.Shuffle(wonders);
+    for (int i = 0; i < numberOfPlayers; i++)
+    {
+        Print($"Type the name of the player {i + 1}:");
+        string playerName = InputString(1);
+        Clear();
+        players.Add(new Player(playerName, Draw(wonders)));
+    }
+    return players;
+}
+
 PrintMenu();
 int value = InputInt(1);
 if (value == 1)
@@ -76,9 +119,13 @@ if (value == 1)
     Print("How many players?");
     int numberOfPlayers = InputInt(1);
     Clear();
+    List<Player> players = GetPlayers(numberOfPlayers);
+    foreach (Player player in players)
+    {
+        Print(player);
+    }
     AddCardsToDeck(deckAgeI, allCards, 1, numberOfPlayers);
     Shuffler.Shuffle(deckAgeI);
     Print(deckAgeI);
-    Print($"Drawed Card:\n{deckAgeI.Draw()}");
 }
 
