62bbe6dca9d5dcf55c8894789fb9d203233210e8
Levi <lnatamm25@gmail.com>
2024-12-01 23:17:58 -0300

    Creating abstract class "AbstractWonder"

---
+++ Models/AbstractWonder.cs
@@ -0,0 +1,27 @@
+﻿namespace SevenWonders.Models;
+
+public abstract class AbstractWonder
+{
+    private string _name;
+    private int _numberOfStages;
+    private List<List<(string, int)>> _stageCosts;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public int NumberOfStages
+    {
+        get => _numberOfStages;
+        set => _numberOfStages = value;
+    }
+
+    public List<List<(string, int)>> StageCosts
+    {
+        get => _stageCosts;
+        set => _stageCosts = value;
+    }
+    
+}
